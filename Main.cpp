c322f7b980502139a6e699736eae76daabf94ecf
Sergi PR <sergiparrar@gmail.com>
2017-03-08 17:36:28 +0100

    Added simple keyboard detection

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,68 @@
 #include "SDL\include\SDL.h"
 
+
 int main(int argc, char* args[]) {
 	SDL_Init(SDL_INIT_EVERYTHING);
 
-	SDL_Window* window;
-	SDL_Renderer* renderer;
-	SDL_CreateWindowAndRenderer(300, 400, 0, &window, &renderer);
+	SDL_Window* window = SDL_CreateWindow("SDL Raiden", 100, 100, 300, 400, NULL);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, 0, SDL_RENDERER_PRESENTVSYNC);
+
 	SDL_Texture* title = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("title.bmp"));
 	SDL_Texture* spaceship = SDL_CreateTextureFromSurface(renderer, SDL_LoadBMP("player.bmp"));
 	SDL_Rect r;
+
+	enum y_State {
+		KEY_DOWN = 0,
+		KEY_UP,
+		KEY_REPEAT
+	};
+
+	int ship_x = 0, ship_y = 0;
+
 
 	bool run = true;
 	while(run) {
 		// Input ------------
 		SDL_Event event;
 		while (SDL_PollEvent(&event)) {
-			if (event.type = SDL_QUIT) {
-				run = false;
+			switch (event.type) {
+			case SDL_QUIT :
+					run = false;
+					break;
+			case SDL_KEYDOWN :
+					switch (event.key.keysym.scancode) {
+					case SDL_SCANCODE_RIGHT:
+						ship_x += 5;
+						break;
+
+					case SDL_SCANCODE_LEFT:
+						ship_x -= 5;
+						break;
+
+					case SDL_SCANCODE_UP:
+						ship_y -= 5;
+						break;
+
+					case SDL_SCANCODE_DOWN:
+						ship_y += 5;
+						break;
+
+					case SDL_SCANCODE_ESCAPE: 
+						run = false;
+						break;
+					}
+					break;
 			}
+
 		}
+		//if (state[])
 
 		// Graphics ------------
 
 		SDL_RenderClear(renderer);
 
 		SDL_RenderCopy(renderer, title, NULL, NULL);
-		r = { 0, 0, 63, 58 };
+		r = { ship_x, ship_y, 63, 58 };
 		SDL_RenderCopy(renderer, spaceship, NULL, &r);
 		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 		SDL_RenderPresent(renderer);
